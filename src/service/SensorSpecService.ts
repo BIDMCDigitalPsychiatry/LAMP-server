806916c68a1cfe1063ba45fd1738e8077557b542
Linoy <linoy.gopal@zcocorporation.com>
2020-07-27 22:23:13 +0530

    #fixes sync between local and couch db via  reverse proxy

--- src/service/SensorSpecService.ts
+++ src/service/SensorSpecService.ts
@@ -1,65 +1,145 @@
-import { Request, Response, Router } from "express"
-import { SensorSpec } from "../model/SensorSpec"
-import { SensorSpecRepository } from "../repository/SensorSpecRepository"
-import { SecurityContext, ActionContext, _verify } from "./Security"
-import jsonata from "jsonata"
+import { Request, Response, Router } from 'express';
+import { SensorSpec } from '../model/SensorSpec';
+import { SensorSpecRepository } from '../repository/SensorSpecRepository';
+import { SecurityContext, ActionContext, _verify } from './Security';
+import { SensorSpecRepository as SensorSpecPouchRepo } from '../repository/pouchRepository/SensorSpecRepository';
+import jsonata from 'jsonata';
 
-export const SensorSpecService = Router()
-SensorSpecService.post("/sensor_spec", async (req: Request, res: Response) => {
+export const SensorSpecService = Router();
+SensorSpecService.post('/sensor_spec', async (req: Request, res: Response) => {
   try {
-    const sensor_spec = req.body
-    const _ = await _verify(req.get("Authorization"), ["self", "sibling", "parent"])
-    const output = { data: await SensorSpecRepository._insert(sensor_spec) }
-    res.json(output)
+    const sensor_spec = req.body;
+    const _ = await _verify(req.get('Authorization'), [
+      'self',
+      'sibling',
+      'parent',
+    ]);
+    const output = { data: await SensorSpecRepository._insert(sensor_spec) };
+    res.json(output);
   } catch (e) {
-    if (e.message === "401.missing-credentials") res.set("WWW-Authenticate", `Basic realm="LAMP" charset="UTF-8"`)
-    res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
+    if (e.message === '401.missing-credentials')
+      res.set('WWW-Authenticate', `Basic realm="LAMP" charset="UTF-8"`);
+    res
+      .status(parseInt(e.message.split('.')[0]) || 500)
+      .json({ error: e.message });
   }
-})
-SensorSpecService.put("/sensor_spec/:sensor_spec_name", async (req: Request, res: Response) => {
+});
+SensorSpecService.put(
+  '/sensor_spec/:sensor_spec_name',
+  async (req: Request, res: Response) => {
+    try {
+      const sensor_spec_name = req.params.sensor_spec_name;
+      const sensor_spec = req.body;
+      const _ = await _verify(req.get('Authorization'), [
+        'self',
+        'sibling',
+        'parent',
+      ]);
+      const output = {
+        data: await SensorSpecRepository._update(sensor_spec_name, sensor_spec),
+      };
+      res.json(output);
+    } catch (e) {
+      if (e.message === '401.missing-credentials')
+        res.set('WWW-Authenticate', `Basic realm="LAMP" charset="UTF-8"`);
+      res
+        .status(parseInt(e.message.split('.')[0]) || 500)
+        .json({ error: e.message });
+    }
+  },
+);
+SensorSpecService.delete(
+  '/sensor_spec/:sensor_spec_name',
+  async (req: Request, res: Response) => {
+    try {
+      const sensor_spec_name = req.params.sensor_spec_name;
+      const _ = await _verify(req.get('Authorization'), [
+        'self',
+        'sibling',
+        'parent',
+      ]);
+      let output = {
+        data: await SensorSpecRepository._delete(sensor_spec_name),
+      };
+      output =
+        typeof req.query.transform === 'string'
+          ? jsonata(req.query.transform).evaluate(output)
+          : output;
+      res.json(output);
+    } catch (e) {
+      if (e.message === '401.missing-credentials')
+        res.set('WWW-Authenticate', `Basic realm="LAMP" charset="UTF-8"`);
+      res
+        .status(parseInt(e.message.split('.')[0]) || 500)
+        .json({ error: e.message });
+    }
+  },
+);
+SensorSpecService.get(
+  '/sensor_spec/:sensor_spec_name',
+  async (req: Request, res: Response) => {
+    try {
+      const sensor_spec_name = req.params.sensor_spec_name;
+      if (process.env.LOCAL_DATA === 'true') {
+        let outputLocal = {
+          data: await SensorSpecPouchRepo._select(sensor_spec_name),
+        };
+        outputLocal =
+          typeof req.query.transform === 'string'
+            ? jsonata(req.query.transform).evaluate(outputLocal)
+            : outputLocal;
+        res.json(outputLocal);
+      } else {
+        const _ = await _verify(req.get('Authorization'), [
+          'self',
+          'sibling',
+          'parent',
+        ]);
+        let output = {
+          data: await SensorSpecRepository._select(sensor_spec_name),
+        };
+        output =
+          typeof req.query.transform === 'string'
+            ? jsonata(req.query.transform).evaluate(output)
+            : output;
+        res.json(output);
+      }
+    } catch (e) {
+      if (e.message === '401.missing-credentials')
+        res.set('WWW-Authenticate', `Basic realm="LAMP" charset="UTF-8"`);
+      res
+        .status(parseInt(e.message.split('.')[0]) || 500)
+        .json({ error: e.message });
+    }
+  },
+);
+SensorSpecService.get('/sensor_spec', async (req: Request, res: Response) => {
   try {
-    const sensor_spec_name = req.params.sensor_spec_name
-    const sensor_spec = req.body
-    const _ = await _verify(req.get("Authorization"), ["self", "sibling", "parent"])
-    const output = { data: await SensorSpecRepository._update(sensor_spec_name, sensor_spec) }
-    res.json(output)
+    if (process.env.LOCAL_DATA === 'true') {
+      let outputLocal = { data: await SensorSpecPouchRepo._select() };
+      outputLocal =
+        typeof req.query.transform === 'string'
+          ? jsonata(req.query.transform).evaluate(outputLocal)
+          : outputLocal;
+      res.json(outputLocal);
+    } else {
+      const _ = await _verify(req.get('Authorization'), [
+        'self',
+        'sibling',
+        'parent',
+      ]);
+      let output = { data: await SensorSpecRepository._select() };
+      output =
+        typeof req.query.transform === 'string'
+          ? jsonata(req.query.transform).evaluate(output)
+          : output;
+      res.json(output);
+    }
   } catch (e) {
-    if (e.message === "401.missing-credentials") res.set("WWW-Authenticate", `Basic realm="LAMP" charset="UTF-8"`)
-    res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
+    if (e.message === '401.missing-credentials')
+      res.set('WWW-Authenticate', `Basic realm="LAMP" charset="UTF-8"`);
+    res
+      .status(parseInt(e.message.split('.')[0]) || 500)
+      .json({ error: e.message });
   }
-})
-SensorSpecService.delete("/sensor_spec/:sensor_spec_name", async (req: Request, res: Response) => {
-  try {
-    const sensor_spec_name = req.params.sensor_spec_name
-    const _ = await _verify(req.get("Authorization"), ["self", "sibling", "parent"])
-    let output = { data: await SensorSpecRepository._delete(sensor_spec_name) }
-    output = typeof req.query.transform === "string" ? jsonata(req.query.transform).evaluate(output) : output
-    res.json(output)
-  } catch (e) {
-    if (e.message === "401.missing-credentials") res.set("WWW-Authenticate", `Basic realm="LAMP" charset="UTF-8"`)
-    res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
-  }
-})
-SensorSpecService.get("/sensor_spec/:sensor_spec_name", async (req: Request, res: Response) => {
-  try {
-    const sensor_spec_name = req.params.sensor_spec_name
-    const _ = await _verify(req.get("Authorization"), ["self", "sibling", "parent"])
-    let output = { data: await SensorSpecRepository._select(sensor_spec_name) }
-    output = typeof req.query.transform === "string" ? jsonata(req.query.transform).evaluate(output) : output
-    res.json(output)
-  } catch (e) {
-    if (e.message === "401.missing-credentials") res.set("WWW-Authenticate", `Basic realm="LAMP" charset="UTF-8"`)
-    res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
-  }
-})
-SensorSpecService.get("/sensor_spec", async (req: Request, res: Response) => {
-  try {
-    const _ = await _verify(req.get("Authorization"), ["self", "sibling", "parent"])
-    let output = { data: await SensorSpecRepository._select() }
-    output = typeof req.query.transform === "string" ? jsonata(req.query.transform).evaluate(output) : output
-    res.json(output)
-  } catch (e) {
-    if (e.message === "401.missing-credentials") res.set("WWW-Authenticate", `Basic realm="LAMP" charset="UTF-8"`)
-    res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
-  }
-})
+});
