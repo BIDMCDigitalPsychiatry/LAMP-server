dffa6d5684a3eb7f5ac4355ae7f3db2900f3a84c
Siju George <sijugeorge@hq.microobjects.net>
2020-08-07 19:37:22 +0530

    ajv validator functionality

--- src/service/ActivityEventService.ts
+++ src/service/ActivityEventService.ts
@@ -22,7 +22,7 @@ ActivityEventService.post("/participant/:participant_id/activity_event", async (
     var validate_request = ajv.validate(request_schema, activity_event)
     if (!validate_request) {
       res.status(500).json({ error: ajv.errorsText() })
-    } 
+    }
     let participant_id = req.params.participant_id
     participant_id = await _verify(req.get("Authorization"), ["self", "sibling", "parent"], participant_id)
     const output = { data: await ActivityEventRepository._insert(participant_id, ae2re(req, [activity_event])) }
@@ -33,7 +33,7 @@ ActivityEventService.post("/participant/:participant_id/activity_event", async (
   }
 })
 ActivityEventService.delete("/participant/:participant_id/activity_event", async (req: Request, res: Response) => {
-  try {    
+  try {
     let ajv = AjvValidator()
     let api_schema: any = await ApiSchemaLAMP()
     let api_params = api_schema.paths["/participant/{participant_id}/activity_event"].get.parameters
@@ -137,13 +137,62 @@ ActivityEventService.get("/participant/:participant_id/activity_event", async (r
 })
 ActivityEventService.get("/study/:study_id/activity_event", async (req: Request, res: Response) => {
   try {
+    let ajv = AjvValidator()
+    let api_schema: any = await ApiSchemaLAMP()
+    let api_params = api_schema.paths["/study/{study_id}/activity_event"].get.parameters
+    let req_properties: any = {}
+    let request_required: any = []
+    api_params.forEach((element: any) => {
+      req_properties[element.name] = element.schema
+      if (element.required !== undefined) {
+        request_required.push(element.name)
+      }
+    })
+    let request_schema: any = {
+      type: "array",
+      items: { type: "object", properties: req_properties },
+    }
+    if (request_required.length > 0) {
+      request_schema.items.required = request_required
+    }
+    let component_schema = {
+      schemas: {
+        Timestamp: api_schema.components.schemas.Timestamp,
+        Identifier: api_schema.components.schemas.Identifier,
+      },
+    }
+    request_schema.components = component_schema
     let study_id = req.params.study_id
+    let req_data = [
+      {
+        study_id: study_id,
+        origin: req.query.origin,
+        from: isNaN(req.query.from) ? req.query.from : parseInt(req.query.from),
+        to: isNaN(req.query.to) ? req.query.to : parseInt(req.query.to),
+        transform: req.query.transform,
+      },
+    ]
+    var validate_request = ajv.validate(request_schema, req_data)
+    if (!validate_request) {
+      res.status(500).json({ error: ajv.errorsText() })
+    }
     const origin: string = req.query.origin
     const from: number | undefined = Number.parse(req.query.from)
     const to: number | undefined = Number.parse(req.query.to)
     const limit = Math.min(Math.max(Number.parse(req.query.limit) ?? 1000, -1000), 1000) // clamped to [-1000, 1000]
     study_id = await _verify(req.get("Authorization"), ["self", "sibling", "parent"], study_id)
     let output = { data: re2ae(req, await ActivityEventRepository._select(study_id, origin, from, to, limit)) }
+    let activity_schema = api_schema.components.schemas.ActivityEvent
+    let response_schema: any = {
+      type: "array",
+      items: activity_schema,
+    }
+    response_schema.items.properties.static_data = activity_schema.properties.data
+    response_schema.components = component_schema
+    var validate_response = ajv.validate(response_schema, output.data)
+    if (!validate_response) {
+      res.status(500).json({ error: ajv.errorsText() })
+    }
     output = typeof req.query.transform === "string" ? jsonata(req.query.transform).evaluate(output) : output
     res.json(output)
   } catch (e) {
@@ -153,13 +202,62 @@ ActivityEventService.get("/study/:study_id/activity_event", async (req: Request,
 })
 ActivityEventService.get("/researcher/:researcher_id/activity_event", async (req: Request, res: Response) => {
   try {
+    let ajv = AjvValidator()
+    let api_schema: any = await ApiSchemaLAMP()
+    let api_params = api_schema.paths["/researcher/{researcher_id}/activity_event"].get.parameters
+    let req_properties: any = {}
+    let request_required: any = []
+    api_params.forEach((element: any) => {
+      req_properties[element.name] = element.schema
+      if (element.required !== undefined) {
+        request_required.push(element.name)
+      }
+    })
+    let request_schema: any = {
+      type: "array",
+      items: { type: "object", properties: req_properties },
+    }
+    if (request_required.length > 0) {
+      request_schema.items.required = request_required
+    }
+    let component_schema = {
+      schemas: {
+        Timestamp: api_schema.components.schemas.Timestamp,
+        Identifier: api_schema.components.schemas.Identifier,
+      },
+    }
+    request_schema.components = component_schema
     let researcher_id = req.params.researcher_id
+    let req_data = [
+      {
+        researcher_id: researcher_id,
+        origin: req.query.origin,
+        from: isNaN(req.query.from) ? req.query.from : parseInt(req.query.from),
+        to: isNaN(req.query.to) ? req.query.to : parseInt(req.query.to),
+        transform: req.query.transform,
+      },
+    ]
+    var validate_request = ajv.validate(request_schema, req_data)
+    if (!validate_request) {
+      res.status(500).json({ error: ajv.errorsText() })
+    }
     const origin: string = req.query.origin
     const from: number | undefined = Number.parse(req.query.from)
     const to: number | undefined = Number.parse(req.query.to)
     const limit = Math.min(Math.max(Number.parse(req.query.limit) ?? 1000, -1000), 1000) // clamped to [-1000, 1000]
     researcher_id = await _verify(req.get("Authorization"), ["self", "sibling", "parent"], researcher_id)
     let output = { data: re2ae(req, await ActivityEventRepository._select(researcher_id, origin, from, to, limit)) }
+    let activity_schema = api_schema.components.schemas.ActivityEvent
+    let response_schema: any = {
+      type: "array",
+      items: activity_schema,
+    }
+    response_schema.items.properties.static_data = activity_schema.properties.data
+    response_schema.components = component_schema
+    var validate_response = ajv.validate(response_schema, output.data)
+    if (!validate_response) {
+      res.status(500).json({ error: ajv.errorsText() })
+    }
     output = typeof req.query.transform === "string" ? jsonata(req.query.transform).evaluate(output) : output
     res.json(output)
   } catch (e) {
@@ -167,7 +265,7 @@ ActivityEventService.get("/researcher/:researcher_id/activity_event", async (req
     res.status(parseInt(e.message.split(".")[0]) || 500).json({ error: e.message })
   }
 })
-
+  
 //////////////////////
 //   [DEPRECATED]   //
 //////////////////////
