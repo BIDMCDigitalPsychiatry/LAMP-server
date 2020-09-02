9af754aff5d29897301f15087f41f20bc21271e6
Linoy <linoy.gopal@zcocorporation.com>
2020-09-02 22:19:49 +0530

    #fixes notification scheduling

--- src/utils/Jobs/ActivitySchedulerJob.ts
+++ src/utils/Jobs/ActivitySchedulerJob.ts
@@ -6,9 +6,21 @@ import { ActivityRepository } from "../../repository/ActivityRepository"
  *
  */
 export const ActivityScheduler = async (participant_id?: string) => {
-  const activity_feed = await ActivityRepository._select(participant_id)
-  console.log(activity_feed)
+  const activity_feed: any = await ActivityRepository._select(participant_id)
 
+  if (activity_feed.length > 0) {
+    activity_feed.map((feed: any) => {
+      if (feed.schedule) {
+        feed.schedule.map((schedule: any) => {
+          notify(feed.name, {
+            start_date: schedule.start_date,
+            time: schedule.time,
+            repeat_interval: schedule.repeat_interval,
+          })
+        })
+      }
+    })
+  }
   //acivity_feed will be processed and individuals with schedule will be
   //passed to ../PushNotifications/ActivitySchedulerNotification. This will be done inside a loop
 }
