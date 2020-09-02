dc0f960694bcda72e7afb5172cff5469e5195869
Linoy <linoy.gopal@zcocorporation.com>
2020-09-02 19:42:47 +0530

    #fixes notifcation scheduler - function prototype

---
+++ src/utils/PushNotifications/ActivitySchedulerNotification.ts
@@ -0,0 +1,25 @@
+const triweekly = [1, 3, 5]
+const biweekly = [2, 4]
+const daily = [0, 1, 2, 3, 4, 5, 6]
+
+/*send notifications to each participant
+*
+*/
+export async function notify(feed:[]) {
+    //1.find device id from sensor_db 
+    //2.send push notification based on repeat interval
+
+//     switch (feed.repeat_interval) {
+//         case "triweekly":
+//             if() {
+
+//             }
+//             break
+//         case "biweekly":
+//             if() {
+
+//              }
+//             break    
+
+// }
+}
