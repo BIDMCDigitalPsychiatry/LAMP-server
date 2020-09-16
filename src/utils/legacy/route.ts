2f42ea212898ea88ffaa32f43728286c6fded392
Siju George <sijugeorge@hq.microobjects.net>
2020-09-16 17:49:27 +0530

    Legacy To Lamp V2 services updated.

--- src/utils/legacy/route.ts
+++ src/utils/legacy/route.ts
@@ -12,6 +12,38 @@ const uuid = customAlphabet("1234567890abcdefghjkmnpqrstvwxyz", 20) // crockford
 
 export const LegacyAPI = Router()
 
+export const SurveyAnswerType = (type: string) => {
+  let AnswerType = ""
+  switch (type) {
+    case "likert":
+      AnswerType = "LikertResponse"
+      break
+    case "list":
+      AnswerType = "ScrollWheels"
+      break
+    case "boolean":
+      AnswerType = "YesNO"
+      break
+    case "clock":
+      AnswerType = "Clock"
+      break
+    case "years":
+      AnswerType = "Years"
+      break
+    case "months":
+      AnswerType = "Months"
+      break
+    case "days":
+      AnswerType = "Days"
+      break
+    case "text":
+      AnswerType = "Textbox"
+      break
+  }
+
+  return AnswerType
+}
+
 /**
  * To convert string from v1 to v2 https://github.com/darkskyapp/string-hash/blob/master/index.js
  * @param id string
@@ -221,17 +253,33 @@ LegacyAPI.post("/SignIn", async (req: Request, res: Response) => {
           UserSettings: {
             AppColor: `${AppColor}`,
             Language: `${defaultLanguage}`,
-            SympSurvey_SlotID:userSettings.UserSettings.SympSurvey_SlotID?userSettings.UserSettings.SympSurvey_SlotID:1,
-            SympSurvey_RepeatID: userSettings.UserSettings.SympSurvey_RepeatID?userSettings.UserSettings.SympSurvey_RepeatID:1,
-            SympSurvey_Time:userSettings.UserSettings.SympSurvey_Time?userSettings.UserSettings.SympSurvey_Time:null,
-            CognTest_SlotID: userSettings.UserSettings.CognTest_SlotID?userSettings.UserSettings.CognTest_SlotID:1,
-            CognTest_RepeatID: userSettings.UserSettings.CognTest_RepeatID?userSettings.UserSettings.CognTest_RepeatID:1,
-            CognTest_Time: userSettings.UserSettings.CognTest_Time?userSettings.UserSettings.CognTest_Time:null,
-            "24By7ContactNo": userSettings.UserSettings["24By7ContactNo"]?userSettings.UserSettings["24By7ContactNo"]:null,
-            PersonalHelpline:userSettings.UserSettings.PersonalHelpline?userSettings.UserSettings.PersonalHelpline:null,
-            PrefferedSurveys:userSettings.UserSettings.PrefferedSurveys?userSettings.UserSettings.PrefferedSurveys:null,
-            PrefferedCognitions: userSettings.UserSettings.PrefferedCognitions?userSettings.UserSettings.PrefferedCognitions:null,
-            Protocol:userSettings.UserSettings.Protocol?userSettings.UserSettings.Protocol:null
+            SympSurvey_SlotID: userSettings.UserSettings.SympSurvey_SlotID
+              ? userSettings.UserSettings.SympSurvey_SlotID
+              : 1,
+            SympSurvey_RepeatID: userSettings.UserSettings.SympSurvey_RepeatID
+              ? userSettings.UserSettings.SympSurvey_RepeatID
+              : 1,
+            SympSurvey_Time: userSettings.UserSettings.SympSurvey_Time
+              ? userSettings.UserSettings.SympSurvey_Time
+              : null,
+            CognTest_SlotID: userSettings.UserSettings.CognTest_SlotID ? userSettings.UserSettings.CognTest_SlotID : 1,
+            CognTest_RepeatID: userSettings.UserSettings.CognTest_RepeatID
+              ? userSettings.UserSettings.CognTest_RepeatID
+              : 1,
+            CognTest_Time: userSettings.UserSettings.CognTest_Time ? userSettings.UserSettings.CognTest_Time : null,
+            "24By7ContactNo": userSettings.UserSettings["24By7ContactNo"]
+              ? userSettings.UserSettings["24By7ContactNo"]
+              : null,
+            PersonalHelpline: userSettings.UserSettings.PersonalHelpline
+              ? userSettings.UserSettings.PersonalHelpline
+              : null,
+            PrefferedSurveys: userSettings.UserSettings.PrefferedSurveys
+              ? userSettings.UserSettings.PrefferedSurveys
+              : null,
+            PrefferedCognitions: userSettings.UserSettings.PrefferedCognitions
+              ? userSettings.UserSettings.PrefferedCognitions
+              : null,
+            Protocol: userSettings.UserSettings.Protocol ? userSettings.UserSettings.Protocol : null,
           },
         })
       } else {
@@ -242,15 +290,15 @@ LegacyAPI.post("/SignIn", async (req: Request, res: Response) => {
             Language: `${defaultLanguage}`,
             SympSurvey_SlotID: 1,
             SympSurvey_RepeatID: 1,
-            SympSurvey_Time:null,
+            SympSurvey_Time: null,
             CognTest_SlotID: 1,
             CognTest_RepeatID: 1,
-            CognTest_Time:null,
+            CognTest_Time: null,
             "24By7ContactNo": "",
             PersonalHelpline: "",
             PrefferedSurveys: "",
             PrefferedCognitions: "",
-            Protocol:""           
+            Protocol: "",
           },
         })
       }
@@ -263,24 +311,23 @@ LegacyAPI.post("/SignIn", async (req: Request, res: Response) => {
           Language: `${defaultLanguage}`,
           SympSurvey_SlotID: 1,
           SympSurvey_RepeatID: 1,
-          SympSurvey_Time:null,
+          SympSurvey_Time: null,
           CognTest_SlotID: 1,
           CognTest_RepeatID: 1,
-          CognTest_Time:null,
+          CognTest_Time: null,
           "24By7ContactNo": "",
           PersonalHelpline: "",
           PrefferedSurveys: "",
           PrefferedCognitions: "",
-          Protocol:""
-         
+          Protocol: "",
         },
       })
       userSettings = await TypeRepository._get("a", ParticipantId, "lamp.legacy_adapter")
-    }  
+    }
 
     //take userid from participant id
     const UserId: APIResponse["UserId"] = ParticipantId.match(/\d+/)[0]
-    
+
     const StudyId: APIResponse["StudyId"] = ParticipantId
     const Type = 0 //non-guest user
     
@@ -293,22 +340,22 @@ LegacyAPI.post("/SignIn", async (req: Request, res: Response) => {
     let SessionToken: APIResponse["SessionToken"] = ""
     const appendedSession = Username + ":" + Password
     SessionToken = Encrypt(appendedSession)
-    Data.UserSettingID=Data.UserID=UserId
-    Data.AppColor=userSettings.UserSettings.AppColor
-    Data.Language=userSettings.UserSettings.Language
-    Data.PersonalHelpline=userSettings.UserSettings.PersonalHelpline
-    Data.PrefferedSurveys=userSettings.UserSettings.PrefferedSurveys
-    Data.PrefferedCognitions=userSettings.UserSettings.PrefferedCognitions
-    Data.Protocol=userSettings.UserSettings.Protocol   
-    Data.SympSurveySlotID=userSettings.UserSettings.SympSurvey_SlotID
-    Data.SympSurveyRepeatID=userSettings.UserSettings.SympSurvey_RepeatID
-    Data.SympSurveySlotTime=userSettings.UserSettings.SympSurvey_Time
-    Data.CognTestSlotID=userSettings.UserSettings.CognTest_SlotID
-    Data.CognTestSlotTime=userSettings.UserSettings.CognTest_Time
-    Data.CognTestRepeatID=userSettings.UserSettings.CognTest_RepeatID
-    Data.ContactNo=userSettings.UserSettings["24By7ContactNo"]
-    Data.ProtocolDate=new Date(0).toISOString().replace(/T/, " ").replace(/..+/, "")
-    
+    Data.UserSettingID = Data.UserID = UserId
+    Data.AppColor = userSettings.UserSettings.AppColor
+    Data.Language = userSettings.UserSettings.Language
+    Data.PersonalHelpline = userSettings.UserSettings.PersonalHelpline
+    Data.PrefferedSurveys = userSettings.UserSettings.PrefferedSurveys
+    Data.PrefferedCognitions = userSettings.UserSettings.PrefferedCognitions
+    Data.Protocol = userSettings.UserSettings.Protocol
+    Data.SympSurveySlotID = userSettings.UserSettings.SympSurvey_SlotID
+    Data.SympSurveyRepeatID = userSettings.UserSettings.SympSurvey_RepeatID
+    Data.SympSurveySlotTime = userSettings.UserSettings.SympSurvey_Time
+    Data.CognTestSlotID = userSettings.UserSettings.CognTest_SlotID
+    Data.CognTestSlotTime = userSettings.UserSettings.CognTest_Time
+    Data.CognTestRepeatID = userSettings.UserSettings.CognTest_RepeatID
+    Data.ContactNo = userSettings.UserSettings["24By7ContactNo"]
+    Data.ProtocolDate = new Date(0).toISOString().replace(/T/, " ").replace(/..+/, "")
+
     //get CTestsFavouriteList
     CTestsFavouriteList = userSettings.UserCTestFavourite ? userSettings.UserCTestFavourite : []
     //get SurveyFavouriteList
@@ -1703,7 +1750,7 @@ LegacyAPI.post("/GetSurveys", [_authorize], async (req: Request, res: Response) 
         settingsObj = {
           QuestionId: settingIndex,
           QuestionText: settingItem.text,
-          AnswerType: settingItem.type,
+          AnswerType: SurveyAnswerType(settingItem.type),
           IsDeleted: false,
           QuestionOptions: questionOptions.length > 0 ? questionOptions : null,
           EnableCustomPopup: false,
