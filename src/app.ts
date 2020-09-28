b4665fa5e2b0efe6c8e482ec5147590c10355fef
Aditya Vaidyam <avaidyam@bidmc.harvard.edu>
2020-09-28 21:16:05 +0000

    Support auto-initialization of the database.

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ import cors from "cors"
 import morgan from "morgan"
 import { customAlphabet } from "nanoid"
 import { LegacyAPI } from "./utils/LegacyAPI"
-import { ActivityScheduler, HTTPS_CERT } from "./utils"
+import { ActivityScheduler, HTTPS_CERT, _bootstrap_db } from "./utils"
 
-//
+// the database
 export const Database = nano(process.env.CDB ?? "")
 // crockford-32
 export const uuid = customAlphabet("1234567890abcdefghjkmnpqrstvwxyz", 20)
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Initialize and configure the application.
 async function main(): Promise<void> {
+  console.group("Initializing LAMP API server...")
   const _openAPIschema = {
     ...(await Database.use("root").get("#schema")),
     _id: undefined,
@@ -44,8 +45,12 @@ async function main(): Promise<void> {
   app.get("/service-worker.js", (req, res) => res.status(204))
   app.all("*", (req, res) => res.status(404).json({ message: "404.api-endpoint-unimplemented" }))
 
+  // Initialize the database or confirm that it is online.
+  await _bootstrap_db(Database)
+
   // Begin running activity/automations scheduling AFTER connecting to the database.
   if (process.env.SCHEDULER === "on") {
+    console.log("Initializing schedulers...")
     setInterval(async () => {
       await ActivityScheduler()
       //await TypeRepository._process_triggers()
@@ -53,11 +58,11 @@ async function main(): Promise<void> {
   } else {
     console.log("Running with schedulers disabled.")
   }
+  console.groupEnd()
+  console.log("Initialization complete.")
 
   // Begin listener on port 3000.
   const _server = process.env.HTTPS === "off" ? app : https.createServer(HTTPS_CERT, app)
   _server.listen(process.env.PORT || 3000)
 }
-
-// GO!
 main()
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import nano from "nano"
+
 export * from "./LegacyAPI"
 export * from "./PushNotification"
 export * from "./ScriptRunner"
@@ -106,4 +108,621 @@ bUFnraLvMJAzLQNN7BrrbdFTot7viPmZYe1Y12unlZ+yqHtusO5AdLF7p0F/t5k/
 R/mQB9d2LJUy81BZrO05VHrz91sHSDRRPg4lDw2GVSFtUE1ILDc3usb7JwJYZIXT
 fARYG40rIsYJipV76ICGNXSp
 -----END CERTIFICATE-----`,
+}
+
+// Initialize the CouchDB databases if any of them do not exist.
+export async function _bootstrap_db(Database: nano.ServerScope): Promise<void> {
+  console.group("Initializing database connection...")
+  const _db_list = await Database.db.list()
+  if (!_db_list.includes("activity_spec")) {
+    console.log("Initializing ActivitySpec database...")
+    await Database.db.create("activity_spec")
+  }
+  console.log("ActivitySpec database online.")
+  if (!_db_list.includes("sensor_spec")) {
+    console.log("Initializing SensorSpec database...")
+    await Database.db.create("sensor_spec")
+  }
+  console.log("SensorSpec database online.")
+  if (!_db_list.includes("researcher")) {
+    console.log("Initializing Researcher database...")
+    await Database.db.create("researcher")
+    Database.use("researcher").bulk({
+      docs: [
+        {
+          _id: "_design/timestamp-index",
+          language: "query",
+          views: {
+            timestamp: {
+              map: {
+                fields: {
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/id-parent-timestamp-index",
+          language: "query",
+          views: {
+            "id-parent-timestamp": {
+              map: {
+                fields: {
+                  _id: "asc",
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["_id", "#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Researcher database online.")
+  if (!_db_list.includes("study")) {
+    console.log("Initializing Study database...")
+    await Database.db.create("study")
+    Database.use("study").bulk({
+      docs: [
+        {
+          _id: "_design/timestamp-index",
+          language: "query",
+          views: {
+            timestamp: {
+              map: {
+                fields: {
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/id-parent-timestamp-index",
+          language: "query",
+          views: {
+            "id-parent-timestamp": {
+              map: {
+                fields: {
+                  _id: "asc",
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["_id", "#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Study database online.")
+  if (!_db_list.includes("participant")) {
+    console.log("Initializing Participant database...")
+    await Database.db.create("participant")
+    Database.use("participant").bulk({
+      docs: [
+        {
+          _id: "_design/timestamp-index",
+          language: "query",
+          views: {
+            timestamp: {
+              map: {
+                fields: {
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/id-parent-timestamp-index",
+          language: "query",
+          views: {
+            "id-parent-timestamp": {
+              map: {
+                fields: {
+                  _id: "asc",
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["_id", "#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Participant database online.")
+  if (!_db_list.includes("activity")) {
+    console.log("Initializing Activity database...")
+    await Database.db.create("activity")
+    Database.use("activity").bulk({
+      docs: [
+        {
+          _id: "_design/timestamp-index",
+          language: "query",
+          views: {
+            timestamp: {
+              map: {
+                fields: {
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/id-parent-timestamp-index",
+          language: "query",
+          views: {
+            "id-parent-timestamp": {
+              map: {
+                fields: {
+                  _id: "asc",
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["_id", "#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Activity database online.")
+  if (!_db_list.includes("sensor")) {
+    console.log("Initializing Sensor database...")
+    await Database.db.create("sensor")
+    Database.use("sensor").bulk({
+      docs: [
+        {
+          _id: "_design/timestamp-index",
+          language: "query",
+          views: {
+            timestamp: {
+              map: {
+                fields: {
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/id-parent-timestamp-index",
+          language: "query",
+          views: {
+            "id-parent-timestamp": {
+              map: {
+                fields: {
+                  _id: "asc",
+                  "#parent": "asc",
+                  timestamp: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["_id", "#parent", "timestamp"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Sensor database online.")
+  if (!_db_list.includes("activity_event")) {
+    console.log("Initializing ActivityEvent database...")
+    await Database.db.create("activity_event")
+    Database.use("activity_event").bulk({
+      docs: [
+        {
+          _id: "_design/parent-activity-timestamp-index",
+          language: "query",
+          views: {
+            "parent-activity-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "desc",
+                  activity: "desc",
+                  timestamp: "desc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: [
+                    {
+                      "#parent": "desc",
+                    },
+                    {
+                      activity: "desc",
+                    },
+                    {
+                      timestamp: "desc",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "desc",
+                  timestamp: "desc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: [
+                    {
+                      "#parent": "desc",
+                    },
+                    {
+                      timestamp: "desc",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("ActivityEvent database online.")
+  if (!_db_list.includes("sensor_event")) {
+    console.log("Initializing SensorEvent database...")
+    await Database.db.create("sensor_event")
+    Database.use("sensor_event").bulk({
+      docs: [
+        {
+          _id: "_design/parent-sensor-timestamp-index",
+          language: "query",
+          views: {
+            "parent-sensor-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "desc",
+                  sensor: "desc",
+                  timestamp: "desc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: [
+                    {
+                      "#parent": "desc",
+                    },
+                    {
+                      sensor: "desc",
+                    },
+                    {
+                      timestamp: "desc",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/parent-timestamp-index",
+          language: "query",
+          views: {
+            "parent-timestamp": {
+              map: {
+                fields: {
+                  "#parent": "desc",
+                  timestamp: "desc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: [
+                    {
+                      "#parent": "desc",
+                    },
+                    {
+                      timestamp: "desc",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("SensorEvent database online.")
+  if (!_db_list.includes("credential")) {
+    console.log("Initializing Credential database...")
+    await Database.db.create("credential")
+    Database.use("sensor_event").bulk({
+      docs: [
+        {
+          _id: "_design/access_key-index",
+          language: "query",
+          views: {
+            access_key: {
+              map: {
+                fields: {
+                  access_key: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["access_key"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/origin-index",
+          language: "query",
+          views: {
+            origin: {
+              map: {
+                fields: {
+                  origin: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["origin"],
+                },
+              },
+            },
+          },
+        },
+        {
+          _id: "_design/origin-access_key-index",
+          language: "query",
+          views: {
+            "origin-access_key": {
+              map: {
+                fields: {
+                  origin: "asc",
+                  access_key: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["origin", "access_key"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Credential database online.")
+  if (!_db_list.includes("tag")) {
+    console.log("Initializing Tag database...")
+    await Database.db.create("tag")
+    Database.use("sensor_event").bulk({
+      docs: [
+        {
+          _id: "_design/parent-type-key-index",
+          language: "query",
+          views: {
+            "parent-type-key": {
+              map: {
+                fields: {
+                  "#parent": "asc",
+                  type: "asc",
+                  key: "asc",
+                },
+                partial_filter_selector: {},
+              },
+              reduce: "_count",
+              options: {
+                def: {
+                  fields: ["#parent", "type", "key"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    })
+  }
+  console.log("Tag database online.")
+  console.groupEnd()
+  console.log("Database verification complete.")
 }
