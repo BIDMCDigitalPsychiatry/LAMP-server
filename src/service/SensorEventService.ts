4c26f68d9541c3cb8ea1640716033fd7bc845672
Linoy <linoy.gopal@zcocorporation.com>
2021-09-18 12:20:43 +0530

    #fixes remove bulk publish

--- src/service/SensorEventService.ts
+++ src/service/SensorEventService.ts
@@ -29,67 +29,10 @@ export class SensorEventService {
 
   public static async create(auth: any, participant_id: string, sensor_events: any[]) {     
     const SensorEventRepository = new Repository().getSensorEventRepository()
-    participant_id = await _verify(auth, ["self", "sibling", "parent"], participant_id)
+    participant_id = await _verify(auth, ["self", "sibling", "parent"], participant_id)    
     const data = await SensorEventRepository._insert(participant_id, sensor_events)
-    //publishing data for activity_event add api((Token will be created in PubSubAPIListenerQueue consumer, as request is assumed as array and token should be created individually)
-    if (sensor_events.length !== 0) {
-      PubSubAPIListenerQueue?.add(
-        {
-          topic: `sensor_event`,
-          action: "create",
-          timestamp: Date.now(),
-          participant_id: participant_id,
-          payload: [sensor_events[sensor_events.length - 1]],
-        },
-        {
-          removeOnComplete: true,
-          removeOnFail: true,
-        }
-      )
-
-      PubSubAPIListenerQueue?.add(
-        {
-          topic: `participant.*.sensor_event`,
-          action: "create",
-          timestamp: Date.now(),
-          participant_id: participant_id,
-          payload: [sensor_events[sensor_events.length - 1]],
-        },
-        {
-          removeOnComplete: true,
-          removeOnFail: true,
-        }
-      )
-
-      PubSubAPIListenerQueue?.add(
-        {
-          topic: `sensor.*.sensor_event`,
-          action: "create",
-          timestamp: Date.now(),
-          participant_id: participant_id,
-          payload: [sensor_events[sensor_events.length - 1]],
-        },
-        {
-          removeOnComplete: true,
-          removeOnFail: true,
-        }
-      )
-
-      PubSubAPIListenerQueue?.add(
-        {
-          topic: `participant.*.sensor.*.sensor_event`,
-          action: "create",
-          timestamp: Date.now(),
-          participant_id: participant_id,
-          payload: [sensor_events[sensor_events.length - 1]],
-        },
-        {
-          removeOnComplete: true,
-          removeOnFail: true,
-        }
-      )
-    }
-
+    BulkDataWrite("sensor_event", participant_id, sensor_events)
+   
     return data
   }
 }
