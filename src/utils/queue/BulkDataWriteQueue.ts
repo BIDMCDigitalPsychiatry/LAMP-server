24ccf7abc255c85580306a20f0d3fe906fa49984
Linoy <linoy.gopal@zcocorporation.com>
2021-09-23 10:53:22 +0530

    #fixes queue for bulk data write

---
+++ src/utils/queue/BulkDataWriteQueue.ts
@@ -0,0 +1,40 @@
+import Bull from "bull"
+import { Repository } from "../../repository/Bootstrap"
+import { Mutex } from "async-mutex"
+import { MongoClientDB } from "../../repository/Bootstrap"
+import { Database } from "../../repository/Bootstrap"
+const clientLock = new Mutex()
+/** Queue Process
+ *
+ * @param job
+ */
+export async function BulkDataWriteQueueProcess(job: Bull.Job<any>): Promise<void> {
+  const repo = new Repository()
+  switch (job.data.key) {
+    case "sensor_event":
+      const SensorEventRepository = repo.getSensorEventRepository()
+      // const release = await clientLock.acquire()      
+      let sensor_events: any[] = []
+      for (const data of job.data.payload) {
+        const participant_id = JSON.parse(data).participant_id
+        const sensor_event = JSON.parse(data)
+        delete sensor_event.participant_id
+        if (process.env.DB?.startsWith("mongodb://")) {
+          await sensor_events.push({ ...sensor_event, _parent: participant_id })
+        } else {
+          await sensor_events.push({ ...sensor_event, "#parent": participant_id })
+        }
+      }
+      try {
+        console.log("sensor_events length",sensor_events.length)
+          await SensorEventRepository._bulkWrite(sensor_events)
+      } catch (error) {
+        console.log(error)
+      }
+      // release()
+      break
+
+    default:
+      break
+  }
+}
