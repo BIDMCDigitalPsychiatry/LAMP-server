1ea0ff9cd4b75a54376fcde53a9adf73112144e6
Facundo Almeida <falmeida@orangeloops.com>
2022-03-22 09:21:46 -0300

    Make /oauth/start endpoint conform to configuration

--- src/service/OAuthService.ts
+++ src/service/OAuthService.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express"
+import { OauthConfiguration } from "../utils/OauthConfiguration"
 import { _verify } from "./Security"
 
 export class OAuthService {
@@ -7,5 +8,6 @@ export class OAuthService {
 }
 
 OAuthService.Router.get("/oauth/start", async (req: Request, res: Response) => {
-  res.redirect("https://google.com")
+  const startURL = new OauthConfiguration().getStartFlowUrl()
+  res.redirect(startURL)
 })
