61ce3e1616b959e9e3f9288f364e813c437c81e5
Mauro Curbelo <mcurbelo@orangeloops.com>
2022-03-24 12:30:55 -0300

    Dev review

--- src/utils/OauthConfiguration.ts
+++ src/utils/OauthConfiguration.ts
@@ -19,14 +19,15 @@ export class OauthConfiguration {
         switch(this.provider) {
             case IdentityProvider.AZURE:
                 let url = new URL(`https://login.microsoftonline.com/${process.env.AZURE_TENANT}/oauth2/v2.0/authorize`)
-                
+
                 this.addParameter(url, "response_type", "token");
                 this.addParameter(url, "response_mode", "query");
-                this.addParameter(url, "clientId", process.env.AZURE_CLIENT_ID);
-                this.addParameter(url, "redirect_uri", process.env.AZURE_REDIRECT_URI);
-                this.addParameter(url, "scope", process.env.AZURE_SCOPE)
-                this.addParameter(url, "state", process.env.AZURE_STATE)
-                this.addParameter(url, "nonce", process.env.AZURE_NONCE)
+                this.addParameter(url, "client_id", process.env.AZURE_CLIENT_ID);
+
+                this.addParameter(url, "redirect_uri", process.env.OAUTH_REDIRECT_URI);
+                this.addParameter(url, "scope", process.env.OAUTH_SCOPE)
+                this.addParameter(url, "state", process.env.OAUTH_STATE)
+                this.addParameter(url, "nonce", process.env.OAUTH_NONCE)
 
                 return url.href;
             case IdentityProvider.GOOGLE:
