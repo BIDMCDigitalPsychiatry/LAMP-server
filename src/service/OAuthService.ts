8e2b9f999ffe5c49ca75f42c2b23e2cc0c52fd14
mcurbelo <maurohcurbelo@gmail.com>
2022-04-01 10:19:56 -0300

    Create endpoint and retrieve tokens from IdP

--- src/service/OAuthService.ts
+++ src/service/OAuthService.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from "express"
 import { OauthConfiguration } from "../utils/OauthConfiguration"
 import { _verify } from "./Security"
-
+import fetch from "node-fetch"
 export class OAuthService {
   public static _name = "OAuth"
   public static Router = Router()
@@ -11,5 +11,33 @@ OAuthService.Router.get("/oauth/start", async (req: Request, res: Response) => {
   const startURL = new OauthConfiguration().getStartFlowUrl()
   res.json({
     "url": startURL
+  })
+})
+
+OAuthService.Router.post("/oauth/authenticate", async (req: Request, res: Response) => {
+  let code = req.body.code
+  let verifier = req.body.code_verifier
+
+  let data = new OauthConfiguration().getRedeemTokenData(code, verifier)
+  let response = await fetch(data.url, {
+    method: "POST",
+    body: data.body
+  })
+
+  response.json().then(json => {
+    if(!!json.error) {
+      res.json({
+        "success": false
+      })
+    } else {
+      res.json({
+        "success": true,
+      })
+    }
+  })
+  .catch(() => {
+    res.json({
+      "success": false
+    })
   })
 })
