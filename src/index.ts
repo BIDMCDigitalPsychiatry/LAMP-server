5c77a2aa8e902ded206dba7e81712afc656f04c1
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2023-11-08 12:29:20 +1100

    feat: Adds signal handlers for server process
    Adds SIGINT and SIGTERM handlers to gracefully shutdown the server see
    (here)[https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html]
    for rationale.
    Tidies up server instantiation. HTTPS must be set to something other than
    "off" to enable HTTPS mode and both the HTTP and HTTPS pathways wrap the
    ExpressJS `app`.
    Adds a warning about running in HTTPS mode as it uses unbuilt TLS certs.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,60 @@
 //require("dotenv").config()
+import http from "http"
 import https from "https"
 import { HTTPS_CERT } from "./utils"
 import { Bootstrap } from "./repository/Bootstrap"
-import app from './app'
+import app from "./app"
+import * as net from "net"
 
 // NodeJS v15+ do not log unhandled promise rejections anymore.
-process.on('unhandledRejection', error => { console.dir(error) })
+process.on("unhandledRejection", error => {
+  console.dir(error)
+})
 
 // Initialize and configure the application.
 async function main(): Promise<void> {
   console.group("Initializing LAMP API server...")
   await Bootstrap()
-  console.log("Server routing initialized.")  
+  console.log("Server routing initialized.")
   console.groupEnd()
   console.log("Initialization complete.")
 
-  // Begin listener on port 3000.
-  const _server = process.env.HTTPS === "off" ? app : https.createServer(HTTPS_CERT, app)
-  _server.listen(process.env.PORT || 3000)
+  const port = process.env.PORT || 3000
+
+  let _server: http.Server | https.Server
+  if (process.env.HTTPS && process.env.HTTPS !== "off") {
+    console.info("Starting HTTPS server on port ", port)
+    console.warn("Server is using default HTTPS certificates. Don't do this.")
+    _server = https.createServer(HTTPS_CERT, app)
+  } else {
+    console.info("Starting HTTP server on port ", port)
+    _server = http.createServer(app)
+  }
+  _server.listen(port)
+
+  /**
+   * Shuts down the server and exists the process.
+   * NB: it does not ensure any clients are cleaned up correctly.
+   */
+  function shutdown() {
+    console.info("LAMP shutting down")
+    _server.close(() => {
+      console.info("LAMP shut down")
+      process.exit(0)
+    })
+  }
+
+  // Add shutdown hook for SIGTERM
+  process.on("SIGTERM", () => {
+    console.debug("SIGTERM received")
+    shutdown()
+  })
+
+  // Add shutdown hook for SIGINT (Ctrl+C)
+  process.on("SIGINT", () => {
+    console.debug("SIGINT received")
+    shutdown()
+  })
 }
-main().then(console.log).catch(console.error)
+
+main().then(() => console.log("Startup complete")).catch(console.error)
