7f897961dfb9540f7783e3f34923814e6bfeecf5
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-11-13 17:50:49 +0530

    removed unwanted js files

--- src/model/Researcher.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Researcher = void 0;
-var Researcher = /** @class */ (function () {
-    function Researcher() {
-    }
-    return Researcher;
-}());
-exports.Researcher = Researcher;
